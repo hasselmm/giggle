da3c13dc29c5223dab2310d54fe5a55e65ada939
Carlos Garnacho <carlosg@gnome.org>
2007-01-12 22:31:42 +0100

    add graph cell renderer skeleton

---
+++ src/giggle-graph-renderer.h
@@ -0,0 +1,33 @@
+#ifndef __GIG_GRAPH_RENDERER_H__
+#define __GIG_GRAPH_RENDERER_H__
+
+G_BEGIN_DECLS
+
+#include <gtk/gtk.h>
+
+#define GIG_TYPE_CELL_RENDERER_GRAPH (gig_cell_renderer_graph_get_type ())
+#define GIG_CELL_RENDERER_GRAPH(obj) (G_TYPE_CHECK_INSTANCE_CAST ((obj), GIG_TYPE_CELL_RENDERER_GRAPH, GigCellRendererGraph))
+#define GIG_CELL_RENDERER_GRAPH_CLASS(klass)	  (G_TYPE_CHECK_CLASS_CAST ((klass), GIG_TYPE_CELL_RENDERER_GRAPH, GigCellRendererGraphClass))
+#define GIG_IS_CELL_RENDERER_GRAPH(obj)	  (G_TYPE_CHECK_INSTANCE_TYPE ((obj), GIG_TYPE_CELL_RENDERER_GRAPH))
+#define GIG_IS_CELL_RENDERER_GRAPH_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), GIG_TYPE_CELL_RENDERER_GRAPH))
+#define GIG_CELL_RENDERER_GRAPH_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), GIG_TYPE_CELL_RENDERER_GRAPH, GigCellRendererGraphClass))
+
+typedef struct GigCellRendererGraph GigCellRendererGraph;
+typedef struct GigCellRendererGraphClass GigCellRendererGraphClass;
+
+struct GigCellRendererGraphClass {
+	GtkCellRendererClass parent_class;
+};
+
+struct GigCellRendererGraph {
+	GtkCellRenderer parent;
+
+	gpointer _priv;
+};
+
+GType		      gig_cell_renderer_graph_get_type (void);
+GigCellRendererGraph *gig_cell_renderer_graph_new      (void);
+
+G_END_DECLS
+
+#endif /* __GIG_GRAPH_RENDERER_H__ */
