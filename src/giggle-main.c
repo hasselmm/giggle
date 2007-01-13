18039059d6db3cf6cb60b93995c4b9105503ddcc
Richard Hult <richard@imendio.com>
2007-01-13 01:30:06 +0100

    Add start of a glade file.

--- src/giggle-main.c
+++ src/giggle-main.c
@@ -1,3 +1,24 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2007 Imendio AB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <config.h>
 #include <gtk/gtk.h>
 
 int
@@ -5,7 +26,19 @@ main (int argc, char **argv)
 {
 	gtk_init (&argc, &argv);
 
+	/*
+	  
+	
+	g_signal_connect (window,
+			  "destroy",
+			  G_CALLBACK (gtk_main_quit),
+			  NULL);
+
+	*/
+	
 	g_print ("Cool things will happen here.\n");
+
+	/* gtk_main (); */
 
 	return 0;
 }
--- src/test-patch-view.c
+++ src/test-patch-view.c
@@ -1,5 +1,24 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2007 Imendio AB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
 #include <config.h>
-#include <gtk/gtk.h>
 #include <gtksourceview/gtksourceview.h>
 #include <gtksourceview/gtksourcelanguagesmanager.h>
 
@@ -51,7 +70,7 @@ giggle_diff_view_new (void)
 	language = gtk_source_languages_manager_get_language_from_mime_type (
 		manager, "text/x-patch");
 
-	gtk_source_buffer_set_language  (GTK_SOURCE_BUFFER (buffer), language);
+	gtk_source_buffer_set_language (GTK_SOURCE_BUFFER (buffer), language);
 	gtk_source_buffer_set_highlight (GTK_SOURCE_BUFFER (buffer), TRUE);
 	
 	g_object_unref (manager);
