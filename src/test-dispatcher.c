09f5f8a6d0e24b838cdd5f8c1b4ad30b1c0f0cab
Mikael Hallendal <micke@imendio.com>
2007-01-13 17:02:36 +0100

    Added a timeout to the test.

--- src/test-dispatcher.c
+++ src/test-dispatcher.c
@@ -1,6 +1,7 @@
 #include "giggle-dispatcher.h"
 
-static GMainLoop *main_loop;
+static GMainLoop        *main_loop;
+static GiggleDispatcher *dispatcher;
 
 static void
 execute_callback (GiggleDispatcher *dispatcher,
@@ -10,7 +11,7 @@ execute_callback (GiggleDispatcher *dispatcher,
 		  gpointer          user_data)
 {
 	if (error) {
-		g_print ("Operation failed: %s", error->message);
+		g_print ("Operation failed: %s\n", error->message);
 		g_main_loop_quit (main_loop);
 		return;
 	}
@@ -23,10 +24,20 @@ execute_callback (GiggleDispatcher *dispatcher,
 	return;
 }
 
+static gboolean
+timeout_cancel (gpointer data)
+{
+	guint id = GPOINTER_TO_INT (data);
+
+	giggle_dispatcher_cancel (dispatcher, id);
+
+	return FALSE;
+}
+
 int
 main (int argc, char **argv)
 {
-	GiggleDispatcher *dispatcher;
+	guint id;
 
 	g_type_init ();
 
@@ -34,16 +45,27 @@ main (int argc, char **argv)
 	
 	main_loop = g_main_loop_new (NULL, FALSE);
 	g_print ("Running execute\n");
-	giggle_dispatcher_execute (dispatcher,
-				   "/home/micke/Source/giggle",
-				   "git diff 9ad6c75b46aa 64456fc212e",
-				   execute_callback,
-				   NULL);
+	id = giggle_dispatcher_execute (dispatcher,
+					"/home/micke/Source/giggle",
+					"git diff 9ad6c75b46aa 64456fc212e",
+					execute_callback,
+					NULL);
+#if 0
+	id = giggle_dispatcher_execute (dispatcher,
+					NULL, "yes", execute_callback, 
+					NULL);
+#endif
+
+	g_timeout_add (10000, timeout_cancel, GINT_TO_POINTER (id));
+
 	g_print ("Waiting for the result\n");
 
 	g_main_loop_run (main_loop);
 	
 	g_print ("Closing down\n");
+
+	g_main_loop_unref (main_loop);
+	g_object_unref (dispatcher);
 
 	return 0;
 }
