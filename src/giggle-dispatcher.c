a038a98f425da8673901dee294379879ca07efd3
Mikael Hallendal <micke@imendio.com>
2007-01-13 02:41:40 +0100

    sliff

--- src/giggle-dispatcher.c
+++ src/giggle-dispatcher.c
@@ -79,8 +79,6 @@ giggle_dispatcher_finalize (GObject *object)
 {
 	GiggleDispatcherPriv *priv = GET_PRIV (object);
 
-	g_queue_foreach (priv->queue, dispatcher_free_job);
-
 	/* FIXME: Free object data */
 
 	G_OBJECT_CLASS (giggle_dispatcher_parent_class)->finalize (object);
