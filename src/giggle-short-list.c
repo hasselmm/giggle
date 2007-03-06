6a1bcc4963361aae903713336deabac5df40deb2
Sven Herzberg <sven@imendio.com>
2007-03-06 11:18:21 +0100

    Started the summary view improvements

---
+++ src/giggle-short-list.c
@@ -0,0 +1,120 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
+/*
+ * Copyright (C) 2007 Imendio AB
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include <config.h>
+
+#include "giggle-short-list.h"
+
+typedef struct GiggleShortListPriv GiggleShortListPriv;
+
+struct GiggleShortListPriv {
+	guint i;
+};
+
+static void     dummy_finalize            (GObject           *object);
+static void     dummy_get_property        (GObject           *object,
+					   guint              param_id,
+					   GValue            *value,
+					   GParamSpec        *pspec);
+static void     dummy_set_property        (GObject           *object,
+					   guint              param_id,
+					   const GValue      *value,
+					   GParamSpec        *pspec);
+
+G_DEFINE_TYPE (GiggleShortList, giggle_short_list, G_TYPE_OBJECT)
+
+#define GET_PRIV(obj) (G_TYPE_INSTANCE_GET_PRIVATE ((obj), GIGGLE_TYPE_DUMMY, GiggleShortListPriv))
+
+static void
+giggle_short_list_class_init (GiggleShortListClass *class)
+{
+	GObjectClass *object_class = G_OBJECT_CLASS (class);
+
+	object_class->finalize     = dummy_finalize;
+	object_class->get_property = dummy_get_property;
+	object_class->set_property = dummy_set_property;
+
+#if 0
+	g_object_class_install_property (object_class,
+					 PROP_MY_PROP,
+					 g_param_spec_string ("my-prop",
+							      "My Prop",
+							      "Describe the property",
+							      NULL,
+							      G_PARAM_READABLE));
+#endif
+
+	g_type_class_add_private (object_class, sizeof (GiggleShortListPriv));
+}
+
+static void
+giggle_short_list_init (GiggleShortList *dummy)
+{
+	GiggleShortListPriv *priv;
+
+	priv = GET_PRIV (dummy);
+}
+
+static void
+dummy_finalize (GObject *object)
+{
+	GiggleShortListPriv *priv;
+
+	priv = GET_PRIV (object);
+	
+	/* FIXME: Free object data */
+
+	G_OBJECT_CLASS (giggle_short_list_parent_class)->finalize (object);
+}
+
+static void
+dummy_get_property (GObject    *object,
+		    guint       param_id,
+		    GValue     *value,
+		    GParamSpec *pspec)
+{
+	GiggleShortListPriv *priv;
+
+	priv = GET_PRIV (object);
+
+	switch (param_id) {
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
+		break;
+	}
+}
+
+static void
+dummy_set_property (GObject      *object,
+		    guint         param_id,
+		    const GValue *value,
+		    GParamSpec   *pspec)
+{
+	GiggleShortListPriv *priv;
+
+	priv = GET_PRIV (object);
+
+	switch (param_id) {
+	default:
+		G_OBJECT_WARN_INVALID_PROPERTY_ID (object, param_id, pspec);
+		break;
+	}
+}
+
