b9c3b02bb95a71c52bdae72dae895b583eb2a6ac
Mikael Hallendal <micke@imendio.com>
2007-09-30 15:28:06 +0200

    Strdup directory from GiggleGit::get_directory before calling set_directory.
    This fixes GGL-78. Patch from \305\201ukasz Jerna\305\233

--- src/giggle-window.c
+++ src/giggle-window.c
@@ -1140,11 +1140,13 @@ window_action_history_refresh (GtkAction    *action,
 			       GiggleWindow *window)
 {
 	GiggleWindowPriv *priv;
-	const gchar      *directory;
+	gchar      *directory;
 
 	priv = GET_PRIV (window);
-	directory = giggle_git_get_directory (priv->git);
+
+	directory = g_strdup (giggle_git_get_directory (priv->git));
 	giggle_window_set_directory (window, directory);
+	g_free (directory);
 }
 
 GtkWidget *
