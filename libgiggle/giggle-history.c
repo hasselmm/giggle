ff8df4b208708041b84ce6bd3ae84f14aa3f52ff
Mathias Hasselmann <hasselmm@gnome.org>
2009-01-23 10:23:31 +0100

    Define a (hopefully) more useful history interface

--- libgiggle/giggle-history.c
+++ libgiggle/giggle-history.c
@@ -1,6 +1,6 @@
 /* -*- Mode: C; tab-width: 8; indent-tabs-mode: t; c-basic-offset: 8 -*- */
 /*
- * Copyright (C) 2007 Imendio AB
+ * Copyright (C) 2009 Mathias Hasselmann
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -22,126 +22,88 @@
 
 enum {
 	HISTORY_CHANGED,
+	HISTORY_RESET,
 	LAST_SIGNAL
 };
 
 static guint signals[LAST_SIGNAL] = { 0, };
 
-static void  giggle_history_base_init    (gpointer  g_class);
-
-GType
-giggle_history_get_type (void)
-{
-  static GType history_type = 0;
-
-  if (G_UNLIKELY (!history_type)) {
-	  const GTypeInfo giggle_history_info = {
-		  sizeof (GiggleHistoryIface), /* class_size */
-		  giggle_history_base_init,    /* base_init */
-		  NULL,		/* base_finalize */
-		  NULL,
-		  NULL,		/* class_finalize */
-		  NULL,		/* class_data */
-		  0,
-		  0,            /* n_preallocs */
-		  NULL
-	  };
-
-	  history_type =
-		  g_type_register_static (G_TYPE_INTERFACE, "GiggleHistory",
-					  &giggle_history_info, 0);
-
-	  g_type_interface_add_prerequisite (history_type, G_TYPE_OBJECT);
-  }
-
-  return history_type;
-}
-
 static void
-giggle_history_base_init (gpointer g_class)
+giggle_history_iface_init (gpointer iface,
+			     gpointer iface_data)
 {
-	static gboolean initialized = FALSE;
-
-	if (initialized) {
-		return;
-	}
-
 	signals[HISTORY_CHANGED] =
 		g_signal_new ("history-changed",
 			      GIGGLE_TYPE_HISTORY,
 			      G_SIGNAL_RUN_LAST,
-			      G_STRUCT_OFFSET (GiggleHistoryIface, history_changed),
-			      NULL, NULL,
-			      g_cclosure_marshal_VOID__VOID,
+			      G_STRUCT_OFFSET (GiggleHistoryIface, changed),
+			      NULL, NULL, g_cclosure_marshal_VOID__VOID,
 			      G_TYPE_NONE, 0);
 
-	initialized = TRUE;
+	signals[HISTORY_RESET] =
+		g_signal_new ("history-reset",
+			      GIGGLE_TYPE_HISTORY,
+			      G_SIGNAL_RUN_LAST,
+			      G_STRUCT_OFFSET (GiggleHistoryIface, reset),
+			      NULL, NULL, g_cclosure_marshal_VOID__VOID,
+			      G_TYPE_NONE, 0);
+}
+
+GType
+giggle_history_get_type (void)
+{
+	static GType type = 0;
+
+	if (G_UNLIKELY (!type)) {
+		type = g_type_register_static_simple
+			(G_TYPE_INTERFACE, "GiggleHistoryIface",
+			 sizeof (GiggleHistoryIface),
+			 giggle_history_iface_init, 0,
+			 NULL, 0);
+
+		g_type_interface_add_prerequisite (type, G_TYPE_OBJECT);
+	}
+
+	return type;
+}
+
+GObject *
+giggle_history_capture (GiggleHistory *history)
+{
+	GiggleHistoryIface *iface;
+
+	g_return_val_if_fail (GIGGLE_IS_HISTORY (history), NULL);
+
+	iface = GIGGLE_HISTORY_GET_IFACE (history);
+	g_return_val_if_fail (NULL != iface->capture, NULL);
+
+	return iface->capture (history);
 }
 
 void
-giggle_history_go_back (GiggleHistory *history)
+giggle_history_restore (GiggleHistory *history,
+			GObject       *snapshot)
 {
 	GiggleHistoryIface *iface;
 
 	g_return_if_fail (GIGGLE_IS_HISTORY (history));
 
 	iface = GIGGLE_HISTORY_GET_IFACE (history);
+	g_return_if_fail (NULL != iface->restore);
 
-	if (iface->go_back) {
-		(iface->go_back) (history);
-	}
-}
-
-gboolean
-giggle_history_can_go_back (GiggleHistory *history)
-{
-	GiggleHistoryIface *iface;
-
-	g_return_val_if_fail (GIGGLE_IS_HISTORY (history), FALSE);
-
-	iface = GIGGLE_HISTORY_GET_IFACE (history);
-
-	if (iface->can_go_back) {
-		return (iface->can_go_back) (history);
-	}
-
-	return FALSE;
-}
-
-void
-giggle_history_go_forward (GiggleHistory *history)
-{
-	GiggleHistoryIface *iface;
-
-	g_return_if_fail (GIGGLE_IS_HISTORY (history));
-
-	iface = GIGGLE_HISTORY_GET_IFACE (history);
-
-	if (iface->go_forward) {
-		(iface->go_forward) (history);
-	}
-}
-
-gboolean
-giggle_history_can_go_forward (GiggleHistory *history)
-{
-	GiggleHistoryIface *iface;
-
-	g_return_val_if_fail (GIGGLE_IS_HISTORY (history), FALSE);
-
-	iface = GIGGLE_HISTORY_GET_IFACE (history);
-
-	if (iface->can_go_forward) {
-		return (iface->can_go_forward) (history);
-	}
-
-	return FALSE;
+	iface->restore (history, snapshot);
 }
 
 void
 giggle_history_changed (GiggleHistory *history)
 {
 	g_return_if_fail (GIGGLE_IS_HISTORY (history));
-
 	g_signal_emit (history, signals[HISTORY_CHANGED], 0);
+}
+
+void
+giggle_history_reset (GiggleHistory *history)
+{
+	g_return_if_fail (GIGGLE_IS_HISTORY (history));
+	g_signal_emit (history, signals[HISTORY_RESET], 0);
 }
